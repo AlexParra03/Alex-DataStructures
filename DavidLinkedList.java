7665b09731d35f887c882dd141fa2ed6abc5f559
Alex <alex.user03@gmail.com>
2017-02-10 18:16:15 -0500

    Methods fixed

--- DavidLinkedList.java
+++ DavidLinkedList.java
@@ -17,19 +17,25 @@ public class DavidLinkedList<E> implements Linkeable<E> {
 	}
 	
 	public DavidLinkedList(){
-		this.root = new Node(null);
-		this.end = this.root;
+                this.root = null;
+                this.end = null;
 		this.size = 0;
 	}
 	
 	// O(1)
 	public boolean add(E element) {
-		Node node = new Node(element);
-		this.end.next = node;
-		this.end = node;
-		size++;
-		
-		return true;
+            if(size == 0){
+                this.root = new Node(element);
+                this.end = this.root;
+                size++;
+                
+            }else{
+                this.end.next = new Node(element);
+                this.end = this.end.next;
+                size++;
+            }
+
+            return true;
 	}
 
 	// O(N)
@@ -37,11 +43,11 @@ public class DavidLinkedList<E> implements Linkeable<E> {
 	public boolean add(int index, E element) {
 		
 		if(index < 0 || index >= size ){
-			return false;
+                    return false;
 		}
 		
 		Node node = root;
-		for(int i=0; i<index; i++){
+		for(int i=0; i<index-1; i++){
 			node = node.next;
 		}
 		
@@ -58,58 +64,72 @@ public class DavidLinkedList<E> implements Linkeable<E> {
 	// O(N)
 	@Override
 	public boolean remove(E element) {
-		Node node = root;
-		
-		while(node.next != null){
-			if(node.next.data.equals(element)){
-				Node skipNext = node.next.next;
-				node.next = skipNext;
-				size--;
-				return true;
-			}
-			node = node.next;
-		}
-		return false;
+            Node node = root;
+            if(root.data.equals(element)){
+                root = root.next;
+                size--;
+            }
+            
+            while(node.next != null){
+                    if(node.next.data.equals(element)){
+                            Node skipNext = node.next.next;
+                            node.next = skipNext;
+                            size--;
+                            return true;
+                    }
+                    node = node.next;
+            }
+            return false;
 	}
 
 	// O(N)
 	@Override
 	public boolean remove(int index) {
-		if(index < 0 || index >= size){
-			return false;
-		}
-		
-		Node node = root;
-		for(int i=0; i<index-1; i++){
-			node = node.next;
-		}
-		
-		Node nextTwo = node.next.next;
-		node.next = nextTwo;
-		return true;
+            if(index < 0 || index >= size || size == 0){
+                    return false;
+            }else if(index == 0){
+                this.root = this.root.next;
+                size--;
+                return true;
+            }else{
+
+                Node node = this.root;
+                for(int i=0; i<index-1; i++){
+                    node = node.next;
+                }
+                node.next = node.next.next;
+                size--;
+                return true;
+            }
 	}
 
 	
 	// O(N)
 	@Override
 	public E get(int index) {
-		Node node = this.root;
-		for(int i=0; i<index; i++){
-			node = node.next;
-		}
-		
-		return node.data;
+            if(index <0 || index >= this.size){
+                return null;
+            }
+            Node node = this.root;
+            for(int i=0; i<index; i++){
+                    node = node.next;
+            }
+
+            return node.data;
 	}
 
 	// O(N)
 	@Override
 	public boolean set(int index, E element) {
-		Node node = root;
-		for(int i=0; i<index; i++){
-			node = node.next;
-		}
-		node.data = element;
-		return true;
+            if(index <0 || index >= this.size){
+                return false;
+            }
+            Node node = root;
+            for(int i=0; i<index-1; i++){
+                    node = node.next;
+            }
+            node.data = element;
+            return true;
 	}
 
 	// O(1)
